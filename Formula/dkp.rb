6feac393b169348e015128aac884a3d5107cd5a7
Matthew Koehr <matt@koehr.com>
2022-12-12 08:33:18 -0500

    Bumped version to 2.3.1

--- Formula/dkp.rb
+++ Formula/dkp.rb
@@ -1,16 +1,19 @@
 class Dkp < Formula
   desc "D2IQ Kubernetes Platform command line tool"
   homepage "https://www.d2iq.com/"
-  version "2.3.0"
+  version "2.3.1"
 
   depends_on "kubernetes-cli"
   depends_on "awscli" => :optional
   
   if OS.mac? && Hardware::CPU.intel?
-    url "https://downloads.d2iq.com/dkp/v2.3.0/dkp_v2.3.0_darwin_amd64.tar.gz"
-    sha256 "65128efe17fa9fab005567000fb0a9421aebc2408117ff89b6e7e164d66edbc5"
+    url "https://downloads.d2iq.com/dkp/v2.3.1/dkp_v2.3.1_darwin_amd64.tar.gz"
+    sha256 "95c87e4a1137a712fff9509724278d6deed6905400fce1d6ba121bac1b6db6f2"
   end
-
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://downloads.d2iq.com/dkp/v2.3.1/dkp_v2.3.1_linux_amd64.tar.gz"
+    sha256 "72128326edfb1299f5bd19f0a64cae8f7d958f7e40d1db21864c61bfbe04a2ac"
+  end
   conflicts_with "dkp"
 
   def install
@@ -22,3 +25,5 @@ class Dkp < Formula
     assert_match "dkp: v#{version}", shell_output("#{bin}/dkp version | grep dkp")
   end
 end
+
+
